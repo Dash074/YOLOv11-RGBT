879674876b7ae04de6e7669eb3661d02f734075b
Dash <Dash074@users.noreply.github.com>
2026-04-24 02:19:07 +0530

    merging custom changes

--- detect-4C.py
+++ detect-4C.py
@@ -5,54 +5,40 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     # image
     '''
-        source 需要和train/val目录一致，且需要包含visible字段，visible同级目录下存在infrared目录，原理是将visible替换为infrared，加载双光谱数据
-        
         The source needs to be in the same directory as the train/val directories, and it must contain the "visible" field. 
         There is an "infrared" directory at the same level as the "visible" directory. 
         The principle is to replace "visible" with "infrared" and load the dual-spectrum data.
     '''
-    model = YOLO(r"runs/LLVIP/LLVIP-yolov5-RGBT-midfusion/weights/best.pt") # select your model.pt path
-    model.predict(source=r'E:\BaiduNetdiskDownload\RGB_IF\LLVIP\LLVIP\images\visible\trainval',
-                  imgsz=640,
-                  project='runs/detect',
-                  name='exp',
-                  show=False,
-                  save_frames=True,
-                  use_simotm="RGBT",
-                  channels=4,
-                  save=True,
-                  # conf=0.2,
-                  # visualize=True # visualize model features maps
-                )
 
-    # OBB task
+    #for 100epoch
+    # model_path = r"C:/Users/harsh/OneDrive/Desktop/college/maverick/AUTONOMOUS/yolo/trained_on_laptops/100epoch_midfusion_b16/weights/best.pt"  # path to model weights
+    # source_path = r"E:/dataset_new/dataset_new/visible/test"  # image folder
+    # project_name = 'runs/100_epoch_evaluation'
+    # run_name = '100_epoch_TMI'
 
-    # model = YOLO(r"runs/dota8/dota8-yolo11n-RGBT-midfusion-obb-e300-16-21/weights/best.pt")  # select your model.pt path
-    # model.predict(source=r'G:\wan\data\dota8\dota8\images\train',
-    #               imgsz=640,
-    #               project='runs/detect',
-    #               name='exp',
-    #               show=False,
-    #               task='obb',
-    #               save_frames=True,
-    #               use_simotm="RGBT",
-    #               channels=4,
-    #               save=True,
-    #               #conf=0.0005,
-    #               # visualize=True # visualize model features maps
-    #               )
+    # model_path = r"C:/Users/harsh/OneDrive/Desktop/college/maverick/AUTONOMOUS/yolo/trained_on_laptops/runs____300EPOCH/200epoch_midfusion_b16/yolo11s-RGBT-midfusion-TMI25____/weights/best.pt"  # path to model weights
+    # source_path = r"E:/dataset_new/dataset_new/visible/test"  # image folder
+    # project_name = 'runs/144_epoch_evaluation'
+    # run_name = '144_epoch_TMI'
+    model_path = r"C:/Users/harsh/OneDrive/Desktop/college/maverick/AUTONOMOUS/yolo/trained_on_laptops/yolo11s-RGBT-midfusion-TMI_300epochs/weights/best.pt"  # path to model weights
+    source_path = r"E:/DroneRGBT_new/visible/test"  # image folder
+    project_name = 'runs/droneRGBT/66_epoch_trail_droneRGBT'
+    run_name = '66_epoch_TMI_trail_DroneRGBT'
 
-    # # VIDEO
-    # model = YOLO(R"runs/M3FD/M3FD-yolov5-RGBT-midfusion/weights/best.pt") # select your model.pt path
-    # model.predict(source=r"G:\wan\data\RGBT\testVD\visible\video.mp4",
-    #               imgsz=640,
-    #               project='runs/detect',
-    #               name='exp',
-    #               show=False,
-    #               save_frames=True,
-    #               use_simotm="RGBT",
-    #               channels=4,
-    #               save=True,
-    #               # conf=0.2,
-    #               # visualize=True # visualize model features maps
-    #             )
+
+    model = YOLO(model_path)
+    model.predict(
+        source=source_path,
+        imgsz=640,
+        project=project_name,
+        name=run_name,
+        # show=True,
+        save=True,
+        save_txt=True,
+        save_conf=True,
+        use_simotm="RGBT",
+        channels=4,
+        conf=0.6,  # adjust threshold as needed
+    )
+
+    
